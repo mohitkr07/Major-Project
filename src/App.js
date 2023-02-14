1a364d7b4d6050b93cbd2dcf4abcf76654f1ce96
AbhishekChorotiya <2020kuec2033@iiitkota.ac.in>
2023-02-14 17:10:50 +0530

    sync

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import "./App.css";
-import AdminLogin from "./components/AdminLogin";
+import './App.css';
+import Main_page from './components/main_page/main_page';
+import { Fragment } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin_dash from './components/admin/admin_dash';
+import AdminLogin from './components/admin/admin_login/admin_login';
 
 function App() {
   return (
-    <>
-      <AdminLogin />
-    </>
-  );
+    <Fragment>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<Main_page/>} />
+                <Route exact path="admin" element={<AdminLogin />} />
+            </Routes>
+        </BrowserRouter>
+    </Fragment>
+);
 }
 
 export default App;

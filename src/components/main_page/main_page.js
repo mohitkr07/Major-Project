d7cfb92c1615bc231db945bac8b7da13dd544b15
Mohit Kumar <82509454+mohitkr07@users.noreply.github.com>
2023-02-15 22:50:51 +0530

    improved

--- src/components/main_page/main_page.js
+++ src/components/main_page/main_page.js
@@ -1,28 +1,21 @@
 import React from "react";
 import Card from "./cards/card";
-import './main_page.css'
+import "./main_page.css";
 
-const Main_page = ()=>{
-
-    return (
-        <div className="outter">
-
-            <div className="navbar">
-
-                <a className="nav_text">Main Page</a>
-
-            </div>
-            <div className="content">
-                <h1 className="heading">Login as ?</h1>
-                <Card text='Student' address='/student'/>
-                <Card text='Faculty' address='/faculty'/>
-                <Card text='Admin' address='/admin'/>
-
-            </div>
-
-        </div>
-    )
-
-}
+const Main_page = () => {
+  return (
+    <div className="outter">
+      <div className="navbar">
+        <a className="nav_text">Main Page</a>
+      </div>
+      <div className="content">
+        <h1 className="heading">Login as ?</h1>
+        <Card text="Student" address="/student" />
+        <Card text="Faculty" address="/faculty" />
+        <Card text="Admin" address="/admin" />
+      </div>
+    </div>
+  );
+};
 
 export default Main_page;

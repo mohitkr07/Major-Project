474cf28911127c8864f8262375601e6d00aa1e22
Mohit Kumar <82509454+mohitkr07@users.noreply.github.com>
2023-02-27 10:31:03 +0530

    initial Student Dashboard

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Admin_dash from "./components/admin/admin_dash";
 import AdminLogin from "./components/admin/admin_login/admin_login";
 import FacultyLogin from "./components/faculty/facultyLogin/FacultyLogin";
 import FacultyReg from "./components/faculty/facultyReg/FacultyReg";
+import StudentDash from "./components/student_dashboard/StudentDash";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="faculty" element={<FacultyLogin />} />
           <Route exact path="adminDashboard" element={<Admin_dash />} />
           <Route exact path="facultyReg" element={<FacultyReg />} />
+          <Route exact path="studentdash" element={<StudentDash />} />
         </Routes>
       </BrowserRouter>
     </Fragment>
---
+++ src/components/student_dashboard/StudentDash.js
@@ -0,0 +1,50 @@
+import React from "react";
+import StudentDashCss from "./StudentDash.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const StudentDash = () => {
+  return (
+    <div class={StudentDashCss.container}>
+      <div class={StudentDashCss.nav}>
+        <span>STUDENT</span>
+      </div>
+
+      <div class={StudentDashCss.semesterBox}>
+        <span id={(StudentDashCss.semHead, StudentDashCss.center)}>
+          Current-Sem
+        </span>
+        <span id={StudentDashCss.semHead}>VI</span>
+      </div>
+
+      <div class={StudentDashCss.front}>
+        <div class={StudentDashCss.options}>
+          <button>Dashboard</button>
+          <button>Courses</button>
+        </div>
+
+        <div class={StudentDashCss.dashboard}>
+          <div class={StudentDashCss.studentInfo}>
+            <div class={StudentDashCss.profile}>
+              <div class={StudentDashCss.profile1}>
+                <div class={StudentDashCss.pic}>
+                  <FontAwesomeIcon icon="fa-4x fa-sharp fa-solid fa-user" />
+                </div>
+              </div>
+              <hr />
+              <div class={StudentDashCss.profile2}>
+                <div class={(StudentDashCss.stId, StudentDashCss.infoBox)}>
+                  <span>Mohit Kumar</span>
+                </div>
+                <div class={(StudentDashCss.stId, StudentDashCss.infoBox)}>
+                  2020KUEC2031
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StudentDash;

543452fe29268292c05e90b4841c2016542aaa71
Mohit Kumar <82509454+mohitkr07@users.noreply.github.com>
2023-04-21 23:45:29 +0530

    toggle button added

--- src/components/faculty/quizzForm/inActive.js
+++ src/components/faculty/quizzForm/inActive.js
@@ -46,6 +46,7 @@ const InActiveQuiz = () => {
               marks={quiz.marks}
             />
           ))}
+          
         </div>
       </div>
     </>
--- src/components/faculty/quizzForm/quizCard.js
+++ src/components/faculty/quizzForm/quizCard.js
@@ -1,34 +1,34 @@
 import React from "react";
-import classes from "./quizCard.module.css"
+import classes from "./quizCard.module.css";
 import { useNavigate } from "react-router-dom";
 
-const Card = (props)=>{
+const Card = (props) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  function handleClick() {
+    navigate(`/addQue?id=${props.id}`);
+  }
 
-    function handleClick(){
-
-        navigate(`/addQue?id=${props.id}`)
-
-    }
-
-    return (
-        <>
-
+  return (
+    <>
+      <div style={{ border: "4px solid black", margin: "10px" }}>
         <button className={classes.out_div} onClick={handleClick}>
-            <div className={classes.inner}>
+          <div className={classes.inner}>
             <a>{props.title}</a>
             <a>{props.faculty}</a>
             <a>{props.year}</a>
             <a>{props.branch}</a>
             <a>{props.duration}</a>
             <a>{props.marks}</a>
-            </div>
-
+          </div>
         </button>
-        
-        </>
-    )
-}
+        <label className={classes.switch}>
+          <input type="checkbox" />
+          <span className={classes.slider}></span>
+        </label>
+      </div>
+    </>
+  );
+};
 
 export default Card;

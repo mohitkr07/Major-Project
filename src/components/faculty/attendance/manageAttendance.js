2727039f420516fc298c0738dddb0ddee95729cf
AbhishekChorotiya <2020kuec2033@iiitkota.ac.in>
2023-05-27 00:59:51 +0530

    done courses fetching

--- src/components/faculty/attendance/manageAttendance.js
+++ src/components/faculty/attendance/manageAttendance.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./courseCard";
 import Styles from "./attendance.module.css";
 let ManageAttendance = () => {
+
+  const [course,setCourse] = useState([]);
+  const [faculty,setFaculty] = useState({});
+
+  useEffect(()=>{
+    getFaculty();
+    getCourses();
+  },[])
+
+  async function getCourses(){
+
+    const res = await fetch(`http://localhost:5000/courses/${faculty.Id}`,{
+      method:'post',
+      credentials: 'include',
+      headers:{
+        "Content-Type": "application/json"
+      },
+    })
+
+    const data = await res.json()
+    setCourse(data)
+    console.log(data)
+
+  }
+
+  async function getFaculty(){
+
+    const res = await fetch('http://localhost:5000/getFaculty',{
+      method:'post',
+      credentials: 'include',
+      headers:{
+        "Content-Type": "application/json"
+      },
+    })
+
+    const data = await res.json()
+    setFaculty(data)
+    console.log(data)
+
+  }
+
+
   return (
     <div className="outter">
       <div className="navbar">
